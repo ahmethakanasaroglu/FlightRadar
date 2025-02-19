31836071b81f53227b5f220a5b3b5c7316a857a2
asarogluahmethakan <asarogluahmethakan@gmail.com>
2025-02-19 09:22:13 +0300

    Initial Commit

---
+++ aTests/aTests.swift
@@ -0,0 +1,17 @@
+//
+//  aTests.swift
+//  aTests
+//
+//  Created by Ahmet Hakan Asaroğlu on 19.02.2025.
+//
+
+import Testing
+@testable import a
+
+struct aTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
